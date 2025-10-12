59ef4a495739b8975ef5116d8196cbd560c8ed25
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2025-10-12 12:30:07 +0300

    cta

--- resources/views/layouts/cta.blade.php
+++ resources/views/layouts/cta.blade.php
@@ -1,11 +1,11 @@
 <section class="py-5 bg-primary text-white">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-8" data-aos="fade-right">
+            <div class="col-lg-8" data-aos="fade-down">
                 <h3 class="mb-3">@lang('front/cta.txt1')</h3>
                 <p class="mb-0">@lang('front/cta.txt2')</p>
             </div>
-            <div class="col-lg-4 text-lg-end" data-aos="fade-left">
+            <div class="col-lg-4 text-lg-end" data-aos="fade-down">
                 <a href="tel:{{ setting('contact', 'phone') }}" class="btn btn-light btn-lg mt-3 mt-lg-0">
                     <i class="fas fa-phone me-2"></i>@lang('front/cta.txt3')
                 </a>
